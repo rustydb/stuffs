e671dd0194f95f958014b85affec1187879b4241
Russell Bunch <rdbunch93@gmail.com>
2016-10-08 21:36:05 -0500

    - Fixed a few typos and spacing things.

--- school/4061-repo/hw0/largearrayinit.c
+++ school/4061-repo/hw0/largearrayinit.c
@@ -1,6 +1,6 @@
-int myarray[50000] = {1, 2, 3, 4} ;
+int myarray[50000] = {1, 2, 3, 4};
 int main(void) {
-	myarray[0] = 3 ;
-	return 0 ;
+	myarray[0] = 3;
+	return 0;
 }
 // Larger executable file size because the array is initialized at compile time
